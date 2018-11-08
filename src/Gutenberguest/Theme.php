ae71805d37a747f229003d21621807aba5c9da31
skhomenko <khomenko.mail@gmail.com>
2018-11-08 17:05:10 +0200

    Theme singleton

--- src/Gutenberguest/Theme.php
+++ src/Gutenberguest/Theme.php
@@ -5,9 +5,22 @@ namespace Gutenberguest;
 class Theme
 {
 
+    private static $instance;
+
     public static function getInstance()
     {
-        echo 'Instance';
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
     }
+
+    /**
+     * Locks for contruct, clone, wakeup methods
+     */
+    public function __construct() {}
+    public function __clone() {}
+    public function __wakeup() {}
 
 }
